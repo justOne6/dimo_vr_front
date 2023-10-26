f6cd54e06d9b643adf8abe49ee9aa4f9b8e36b5f
Meryem Ben Jouda <miryambjzidi@gmail.com>
2023-10-26 23:45:31 +0200

    sign in page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="signIn">
+    <div>
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required />
+    </div>
+    <div>
+      <button type="submit">Sign In</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    signIn() {
+      console.log('Sign in with email:', this.email, 'and password:', this.password);
+    },
+  },
+};
+</script>
