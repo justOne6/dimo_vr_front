fcb8b507c9b4e93f22580e4e1a68194798abe5ff
Meryem Ben Jouda <miryambjzidi@gmail.com>
2023-11-03 15:43:51 +0100

    added axios and route for the sigin method

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,15 +3,23 @@
     <DimoVR />
     <div class="inputs">
       <p class="create">Sign In</p>
-      <input class="input" type="text" placeholder="Email" />
-      <input class="input" type="text" placeholder="Password" />
+      <input class="input" type="text" placeholder="Email" v-model="email" />
+      <input
+        class="input"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
     </div>
     <div class="signup">
-      <v-btn class="signup_button"><span>SIGN IN</span></v-btn>
+      <v-btn class="signin_button" @click="signIn"><span>SIGN IN</span></v-btn>
     </div>
     <div style="margin: 2% auto; width: fit-content">
-      <a href="/forgotten-password" style="color: #2A6A8A; font-weight: bold; text-decoration: none;">Forgot password
-        ?</a>
+      <a
+        href="/forgotten-password"
+        style="color: #2a6a8a; font-weight: bold; text-decoration: none"
+        >Forgot password ?</a
+      >
     </div>
   </div>
 </template>
@@ -19,6 +27,8 @@
 <script>
 import { generalColors } from "../../globalVars";
 import DimoVR from "@/components/DimoVR.vue";
+import axios from "axios";
+
 export default {
   components: {
     DimoVR,
@@ -30,7 +40,37 @@ export default {
       mediumGrey: generalColors.mediumGrey,
       boldOrange: generalColors.boldOrange,
       lightOrange: generalColors.lightOrange,
+      email: "",
+      password: "",
     };
+  },
+  methods: {
+    signIn() {
+      const loginData = {
+        username: this.email,
+        password: this.password,
+      };
+
+      axios
+        .post("http://127.0.0.1:8000/api/login_check", loginData)
+        .then((response) => {
+          if (response.data.token) {
+            // Stock jeton JWT dans le localStorage
+            localStorage.setItem("jwtToken", response.data.token);
+            // router.push("/dashboard");
+
+            console.log(
+              "Connexion réussie, jeton JWT stocké:",
+              response.data.token
+            );
+          } else {
+            console.error("Le serveur n'a pas renvoyé de jeton.");
+          }
+        })
+        .catch((error) => {
+          console.error("Erreur de connexion:", error);
+        });
+    },
   },
 };
 </script>
