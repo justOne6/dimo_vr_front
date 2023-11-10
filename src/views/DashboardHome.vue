832669b0d09d2cd799f68f35c17a00e984281241
Meryem Ben Jouda <miryambjzidi@gmail.com>
2023-11-10 17:20:39 +0100

    sign in is functionnal and I can sign in and go to dashboard page

---
+++ src/views/DashboardHome.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main">
+    <div class="right">
+      <DimoVR />
+    </div>
+    <h1 class="title">Helooo</h1>
+  </div>
+</template>
+
+<script>
+import { generalColors } from "../../globalVars";
+import DimoVR from "@/components/DimoVR.vue";
+export default {
+  components: { DimoVR },
+  data() {
+    return {
+      medimuBlue: generalColors.mediumBlue,
+      darkBlue: generalColors.darkBlue,
+      mediumGrey: generalColors.mediumGrey,
+      boldOrange: generalColors.boldOrange,
+      lightOrange: generalColors.lightOrange,
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #f0f0f0;
+}
+
+.main {
+  font-family: "Raleway", sans-serif;
+  margin-top: 8%;
+  margin-bottom: auto;
+}
+
+.edits {
+  margin: 10% auto;
+  font-size: 18px;
+}
+
+.dimo {
+  color: var(--lightOrange);
+  font-weight: bold;
+}
+
+.title {
+  font-size: 48px;
+  margin: 0 auto !important;
+  width: fit-content;
+}
+</style>
