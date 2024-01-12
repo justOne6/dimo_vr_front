eb93578b37b9a05670c1154dfc527e8a107f4bf1
Meryem Ben Jouda <miryambjzidi@gmail.com>
2024-01-12 11:27:29 +0100

    classroom navbar added + css

--- src/views/ClassRoom.vue
+++ src/views/ClassRoom.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
+  <div class="main">
     <Navbar />
-
     <h1>Welcome to your {{ $route.params.label }} classroom!</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import Navbar from "@/components/Navbar.vue";
+
+export default { components: { Navbar } };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main {
+  margin-top: 80px !important;
+  font-family: "Raleway", sans-serif !important;
+}
+</style>
