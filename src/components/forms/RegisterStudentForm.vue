76aee2c5058129cadee0208418fe288fc6d1bc3a
Quentin Tenga <quentin@MacBook-Pro-de-Quentin-2.local>
2024-04-19 16:22:10 +0200

    refonte

--- src/components/forms/RegisterStudentForm.vue
+++ src/components/forms/RegisterStudentForm.vue
@@ -8,14 +8,23 @@
         Le nouvel étudiant a bien été enregistré.
       </div>
       <div style="margin-top:20px">
-        <p class="create">Add a new student</p>
-        <v-text-field outlined clearable class="input" label="First Name" v-model="firstname"></v-text-field>
-        <v-text-field outlined clearable class="input" label="Last Name" v-model="lastname"></v-text-field>
-        <v-text-field outlined clearable class="input" label="Email" v-model="email" type="email"></v-text-field>
+        <p class="page_title">Add a new student</p>
+        <div class="input_container">
+          <p class="input_title">First name</p>
+          <input class="input" label="Enter text..." v-model="firstname" placeholder="Enter first name" />
+        </div>
+        <div class="input_container">
+          <p class="input_title">Last name</p>
+          <input class="input" label="Enter text..." v-model="lastname" placeholder="Enter last name" />
+        </div>
+        <div class="input_container">
+          <p class="input_title">Email</p>
+          <input class="input" label="Enter text..." v-model="email" placeholder="Enter email" />
+        </div>
       </div>
       <br />
       <div class="signup">
-        <v-btn class="signup_button" @click="signUp"><span>Create student account</span></v-btn>
+        <v-btn class="signup_button" @click="signUp"><span>Create</span></v-btn>
       </div>
     </div>
   </div>
@@ -23,7 +32,7 @@
 
 <script>
 import axios from "axios";
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 
 export default {
   name: "RegisterTeacherForm",
@@ -48,32 +57,32 @@ export default {
       };
 
       axios
-          .post(`${process.env.VUE_APP_API_URI}/api/register-student`, userData, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          })
-          .then((response) => {
-            console.log("Response: ", response);
-            if (response.status === 200) {
-              this.registrationSuccess = true;
-              // efface les champs
-              this.lastname = "";
-              this.firstname = "";
-              this.email = "";
+        .post(`${process.env.VUE_APP_API_URI}/api/register-student`, userData, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((response) => {
+          console.log("Response: ", response);
+          if (response.status === 200) {
+            this.registrationSuccess = true;
+            // efface les champs
+            this.lastname = "";
+            this.firstname = "";
+            this.email = "";
+          }
+        })
+        .catch((error) => {
+          console.error("Error while registering new teacher: ", error)
+          if (error.response) {
+            if (error.response.status === 500) {
+              this.registrationError = error?.response?.data.message;
             }
-          })
-          .catch((error) => {
-            console.error("Error while registering new teacher: ", error)
-            if (error.response) {
-              if (error.response.status === 500) {
-                this.registrationError = error?.response?.data.message;
-              }
-            }
-            setTimeout(() => {
-              this.registrationError = null;
-            }, 5000);
-          });
+          }
+          setTimeout(() => {
+            this.registrationError = null;
+          }, 5000);
+        });
     },
   },
 };
