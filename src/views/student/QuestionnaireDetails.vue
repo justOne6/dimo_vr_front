fbf67a7d1a6fb3fe8fb3cccef8fb76dd865917b7
Meryem Ben Jouda <miryambjzidi@gmail.com>
2024-06-11 21:46:32 +0200

    rules changes

--- src/views/student/QuestionnaireDetails.vue
+++ src/views/student/QuestionnaireDetails.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="page-restrict-width questionnaire-wrapper">
     <p v-if="!isAlreadyTreated">
-      Toute bonne réponse vous rapporte 1 point.<br>
-      Toute mauvaise réponse vous enlève 1 point.<br>
-      Vous pouvez donc obtenir un score négatif.<br>
+      Toute bonne réponse vous rapporte 1 point.<br />
+      Les mauvaises réponses ne vous enlèvent pas de points.<br />
+      Vous ne pouvez donc pas obtenir un score négatif et vous aurez un score de
+      0 si vous n'avez aucune bonne réponse.<br />
       Votre résultat sera affiché à la fin du questionnaire en pourcentage.
     </p>
-    <h5 class="resultat" v-if="isAlreadyTreated">Vous avez obtenu un score de {{this.score}}% à ce questionnaire. </h5>
+    <h5 class="resultat" v-if="isAlreadyTreated">
+      Vous avez obtenu un score de {{ this.score }}% à ce questionnaire.
+    </h5>
     <v-card>
       <v-card-title class="text-h2" v-text="questionnaire.title"></v-card-title>
       <v-card-text v-text="questionnaire.description"></v-card-text>
       <v-divider></v-divider>
       <div>
-          <div
-            v-for="question in questionnaire.questions"
-            :key="question.id"
-          >
-            <h3>{{question.text}}</h3>
-            <ul>
-                <v-checkbox
-                    v-for="option in question.options"
-                    :key="option.id"
-                    :label="option.text"
-                    v-model="option.selected"
-                    :disabled="isAlreadyTreated"
-                ></v-checkbox>
-            </ul>
-          </div>
+        <div v-for="question in questionnaire.questions" :key="question.id">
+          <h3>{{ question.text }}</h3>
+          <ul>
+            <v-checkbox
+              v-for="option in question.options"
+              :key="option.id"
+              :label="option.text"
+              v-model="option.selected"
+              :disabled="isAlreadyTreated"
+            ></v-checkbox>
+          </ul>
+        </div>
       </div>
       <v-divider></v-divider>
-      <v-btn v-if="!isAlreadyTreated" @click="saveAnswers" color="primary">Envoyer les réponses</v-btn>
+      <v-btn v-if="!isAlreadyTreated" @click="saveAnswers" color="primary"
+        >Envoyer les réponses</v-btn
+      >
     </v-card>
-
-
   </div>
 </template>
 
@@ -46,13 +46,11 @@ export default {
       required: true,
     },
   },
-  components: {
-
-  },
-data() {
+  components: {},
+  data() {
     return {
       questionnaire: {},
-      isAlreadyTreated : false,
+      isAlreadyTreated: false,
       score: 0,
     };
   },
@@ -62,22 +60,21 @@ data() {
   methods: {
     // Vérifier si l'élève a déjà répondu à ce questionnaire
     async checkWhatToDisplay() {
-      try{
+      try {
         // Vérifier si l'élève a déjà répondu à ce questionnaire
         const alreadyTreated = await axios.get(
-            `${process.env.VUE_APP_API_URI}/api/treated/${this.questionnaireId}`,
-            {
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-              },
-            }
+          `${process.env.VUE_APP_API_URI}/api/treated/${this.questionnaireId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
         );
         // Caster les résultats en tableau
         let data = alreadyTreated.data;
         // Si l'élève a déjà répondu à ce questionnaire l'afficher avec ses réponses
         await this.displayQuestionnaireWithAnswers(data);
-      }
-      catch (error) {
+      } catch (error) {
         // si c'est une erreur 404, c'est que l'élève n'a pas encore répondu à ce questionnaire
         if (error.response.status === 404) {
           // Récupérer les données du questionnaire à remplir
@@ -86,20 +83,22 @@ data() {
           console.error("Error checking what to display:", error);
         }
       }
-
     },
-    displayQuestionnaireWithAnswers(data){
+    displayQuestionnaireWithAnswers(data) {
       console.log(data);
       this.isAlreadyTreated = true;
       this.score = data.score;
       this.questionnaire = data.questionnaire;
       // Ajouter une propriété pour stocker les réponses de l'utilisateur
-      this.questionnaire.questions.forEach(question => {
-        question.options.forEach(option => {
-          this.$set(option, "selected", data.answers.some(answer => answer.option_id === option.id));
+      this.questionnaire.questions.forEach((question) => {
+        question.options.forEach((option) => {
+          this.$set(
+            option,
+            "selected",
+            data.answers.some((answer) => answer.option_id === option.id)
+          );
         });
       });
-
     },
     // Récupérer les données du questionnaire
     async fetchQuestionnaire() {
@@ -114,8 +113,8 @@ data() {
         );
         this.questionnaire = response.data.questionnaire;
         // Ajouter une propriété pour stocker les réponses de l'utilisateur
-        this.questionnaire.questions.forEach(question => {
-          question.options.forEach(option => {
+        this.questionnaire.questions.forEach((question) => {
+          question.options.forEach((option) => {
             this.$set(option, "selected", false);
           });
         });
@@ -128,21 +127,22 @@ data() {
       // console.log("Réponses du questionnaire :", this.questionnaire);
       try {
         await axios.post(
-            `${process.env.VUE_APP_API_URI}/api/questionnaires/answers`,
-            this.questionnaire,
-            {
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-              },
-            }
+          `${process.env.VUE_APP_API_URI}/api/questionnaires/answers`,
+          this.questionnaire,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
         );
         // Raffraichir la page
         await this.checkWhatToDisplay();
-      }
-      catch (error) {
+      } catch (error) {
         // si un code 400, c'est que le formulaire est déjà rempli
         if (error.response.status === 400) {
-          alert("Vous ne pouvez pas répondre à ce questionnaire plusieurs fois.");
+          alert(
+            "Vous ne pouvez pas répondre à ce questionnaire plusieurs fois."
+          );
         } else {
           console.error("Error saving answers:", error);
         }
@@ -156,7 +156,7 @@ data() {
 .questionnaire-wrapper {
   margin-top: 7rem;
 }
-.questionnaire-wrapper .v-list-item{
+.questionnaire-wrapper .v-list-item {
   flex-direction: column;
 }
 </style>
